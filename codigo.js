d6b763bcf65cc40719c7920c48cba79108e88ac4
marcosfransa <96637179+marcosfransa@users.noreply.github.com>
2022-03-10 08:37:38 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -7,9 +7,9 @@ if (dispositivo) var caminho = "/storage/emulated/0/.contas"; else var caminho =
 
 var banco = "contas";
 var MinhaData = new Date();
-var Dia = MinhaData.getDate();
-var Mes = MinhaData.getMonth()+1;
-var Mez = Mes;
+var Dia = 28;//MinhaData.getDate();
+var Mes = 02;//MinhaData.getMonth()+1;
+var Mez = 02;//Mes;
 var Ano = MinhaData.getFullYear();
 var HH = MinhaData.getHours();
 var MM = MinhaData.getMinutes();
