c0545c14c8dbdd1924017ce0af3993e29bc60db3
marcosfransa <96637179+marcosfransa@users.noreply.github.com>
2024-03-01 06:32:31 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -77,6 +77,23 @@ function Zerado(n,len,padding) {
 	return sign + s;
 }
 
+const mascaraMoeda = (event) => {
+  const onlyDigits = event.target.value
+    .split("")
+    .filter(s => /\d/.test(s))
+    .join("")
+    .padStart(3, "0")
+  const digitsFloat = onlyDigits.slice(0, -2) + "." + onlyDigits.slice(-2)
+  event.target.value = maskCurrency(digitsFloat)
+}
+
+const maskCurrency = (valor, locale = 'pt-BR', currency = 'BRL') => {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  }).format(valor)
+}
+
 function formatamoeda(num){
 	x = 0;
     if (num<0){
